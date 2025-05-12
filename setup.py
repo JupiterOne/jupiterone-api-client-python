0ffecca92daaeb882cc61b2bd9cdba3975dd356e
SeaBlooms <colinblumer@gmail.com>
2025-05-12 11:13:28 -0600

    version="1.4.0"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_reqs = ["requests", "retrying"]
 
 setup(
     name="jupiterone",
-    version="1.3.0",
+    version="1.4.0",
     description="A Python client for the JupiterOne API",
     license="MIT License",
     author="JupiterOne",
