8dc06841fedcf4a4bddeea6193c48b508f7bc544
hmcguire443 <hunter.mcguire@jupiterone.com>
2025-11-06 13:12:25 -0600

    Bump Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_reqs = ["requests", "retrying"]
 
 setup(
     name="jupiterone",
-    version="2.0.1",
+    version="2.1.0",
     description="A Python client for the JupiterOne API",
     license="MIT License",
     author="JupiterOne",
